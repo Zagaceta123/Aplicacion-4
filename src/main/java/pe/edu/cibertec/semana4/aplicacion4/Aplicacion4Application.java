6f510e133f20c9297d221e3d894e6190aa70a073
Zagaceta123 <rz_sebas@hotmail.com>
2023-09-17 13:38:08 -0500

    nuevo

---
+++ src/main/java/pe/edu/cibertec/semana4/aplicacion4/Aplicacion4Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.semana4.aplicacion4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplicacion4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aplicacion4Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/semana4/aplicacion4/Aplicacion4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.semana4.aplicacion4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aplicacion4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
