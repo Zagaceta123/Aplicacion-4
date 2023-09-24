f4a2c13b0105b0ad6b2da67903d63c847bc561ea
Zagaceta123 <rz_sebas@hotmail.com>
2023-09-24 16:17:58 -0500

    ejercicio primo

---
+++ src/main/java/pe/edu/cibertec/semana4/aplicacion4/model/PrimoModel.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.semana4.aplicacion4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrimoModel {
+    private Integer numero;
+}
